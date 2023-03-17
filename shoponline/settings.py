61c1b15a889aff6d3324a3dcb8d78dcd66a42bc5
unuefeejovwoke <vuwill7114@gmail.com>
2023-03-17 23:17:33 +0100

    userprofile model

--- shoponline/settings.py
+++ shoponline/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'shoponline/static'
 ] 
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
