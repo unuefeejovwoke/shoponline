d501d8918df08d24f785804dbedae6d982c6216b
unuefeejovwoke <vuwill7114@gmail.com>
2023-03-18 21:14:47 +0100

    django signals for user profile and user register template setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def registerUser(request):
+    return render(request, 'accounts/registerUser.html')
