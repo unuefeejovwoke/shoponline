0735ab87cc8d3540f7d753b0376822e98215acef
unuefeejovwoke <vuwill7114@gmail.com>
2023-03-26 22:01:15 +0100

    login validation feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
     
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
+
 ]
