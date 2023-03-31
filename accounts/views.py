3e09206cd8e1b1d71a49d0de0a92bb7a5e56dced
unuefeejovwoke <vuwill7114@gmail.com>
2023-03-31 21:03:25 +0100

    set up user and vendor profile pic and cover dynamically

--- accounts/views.py
+++ accounts/views.py
@@ -178,8 +178,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-   
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    context ={
+        'vendor':vendor,
+    }
+    return render(request, 'accounts/vendorDashboard.html', context)
 
 def forgot_password(request):
     if request.method == 'POST':
