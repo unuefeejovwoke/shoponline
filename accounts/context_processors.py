46700ebf93911fa847613204203b515768cde34c
unuefeejovwoke <vuwill7114@gmail.com>
2023-04-02 21:38:11 +0100

    user and vendor dashboard edits

---
+++ accounts/context_processors.py
@@ -0,0 +1,9 @@
+from vendor.models import Vendor
+from django.conf import settings
+
+def get_vendor(request):
+    try:
+        vendor = Vendor.objects.get(user=request.user)
+    except:
+        vendor = None
+    return dict(vendor=vendor)
