2fb2e8f1eeb085f953492d14015169ff4a1282be
unuefeejovwoke <vuwill7114@gmail.com>
2023-04-04 00:54:03 +0100

    update vendor profle feature

--- vendor/forms.py
+++ vendor/forms.py
@@ -4,7 +4,7 @@ from .models import Vendor
 
 
 class VendorForm(forms.ModelForm):
-    # vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
