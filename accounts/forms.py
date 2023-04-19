4ccd7082b59c315d036964b9725d1dc574c05bd9
unuefeejovwoke <vuwill7114@gmail.com>
2023-04-19 19:08:28 +0100

    field validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import User, UserProfile
-# from .validators import allow_only_images_validator
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -22,8 +22,8 @@ class UserForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
-    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
-    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     
     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
