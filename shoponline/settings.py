2f4016a6bd90a8b6a7d9d143fe470cb5ec063268
unuefe ejovwoke <vuwill7114@gmail.com>
2023-05-08 11:08:39 -0700

    connected to postgresql and fixed new issues

--- shoponline/settings.py
+++ shoponline/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'judvc^n%g_iip3*1y%!g7*g5dr#*s^7q$k95(6(86f-!y%ff*_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
