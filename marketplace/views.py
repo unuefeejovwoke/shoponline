c17221c4ee8493e8668e3a3ac986c252907066e5
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-08 11:42:54 -0700

    cart counter context processor

--- marketplace/views.py
+++ marketplace/views.py
@@ -3,8 +3,8 @@ from vendor.models import Vendor
 from django.db.models import Prefetch
 from menu.models import Category, FoodItem
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Cart
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -47,5 +47,26 @@ def vendor_detail(request, vendor_slug):
     return render(request, 'marketplace/vendor_detail.html', context)
 
 
-def add_to_cart(request, food_id): 
-    return HttpResponse("Testing")
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity'}) #, 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart'}) #, 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+        
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
--- shoponline/static/js/custom.js
+++ shoponline/static/js/custom.js
@@ -86,7 +86,7 @@ $(document).ready(function(){
             url: url,
             data: data,
             success: function(response){
-                alert(response);
+                console.log(response)
             }
         })
     })
